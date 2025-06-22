9ee19f2dd1a6e04355948b133f3612cdc29a005e
DanielNieves04 <aleurba30@gmail.com>
2025-06-21 22:42:26 -0500

    Correcciones

---
+++ controlDeTareasBackend/src/main/java/com/Tareas/ControlTareas/HealthController.java
@@ -0,0 +1,13 @@
+package com.Tareas.ControlTareas;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+     @GetMapping("/")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
