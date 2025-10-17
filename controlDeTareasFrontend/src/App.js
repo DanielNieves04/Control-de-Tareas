4c957a99553c647ec636a20246214e3252631ac8
DanielNieves04 <aleurba30@gmail.com>
2025-10-17 12:48:45 -0500

    Subiendo a Render

--- controlDeTareasFrontend/src/App.js
+++ controlDeTareasFrontend/src/App.js
@@ -95,7 +95,7 @@ function App() {
     setMensajesChat(prev => [...prev, { tipo: 'usuario', texto: mensaje }]);
     setMensaje('');
 
-    fetch("http://localhost:5000/generar", {
+    fetch("https://control-de-tareas-pythonapp.onrender.com/generar", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -146,7 +146,7 @@ function App() {
       }
     };
 
-    fetch("http://localhost:8000/tareas/saveTarea", {
+    fetch("https://control-de-tareas-backend.onrender.com/tareas/saveTarea", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -167,7 +167,7 @@ function App() {
   const detectarTareasDuplicadas = () => {
     if (!tareas.length) return;
 
-    fetch("http://localhost:5000/detectar-duplicadas", {
+    fetch("https://control-de-tareas-pythonapp.onrender.com/detectar-duplicadas", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ tareas: tareas.map(t => t.tarea) })
